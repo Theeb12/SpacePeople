cffc896b854722cb43d8b236736a0a173d644e7f
Jason Don <jdon@crunchbase.com>
2024-06-16 00:25:45 -0700

    multiplayer relay working

--- Assets/Scripts/ClientNetworkTransform.cs
+++ Assets/Scripts/ClientNetworkTransform.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.Netcode.Components;
 using UnityEngine;
 
+// Just an override class on NetworkTransform to give clients full access
+// if no override, they won't be able to move objects on the host
 public class ClientNetworkTransform : NetworkTransform {
     protected override bool OnIsServerAuthoritative()
     {
--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,36 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Unity.Netcode;
 using System;
+using Unity.Services.Relay;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using Unity.Services.Relay.Models;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using UnityEngine.UI;
+using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] private Button serverButton;
     [SerializeField] private Button clientButton;
     [SerializeField] private Button hostButton;
+
+    [SerializeField] private TMP_InputField joinCodeInputField;
+    [SerializeField] private Button joinCodeButton;
     
     // Start is called before the first frame update
-    void Start()
-    {
-        serverButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartServer();
-        });
-        clientButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartClient();
-        });
-        hostButton.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartHost();
-        });
-    }
+    void Start() {
+        clientButton.onClick.AddListener(() => {
+            joinCodeInputField.gameObject.SetActive(true);
+            joinCodeButton.gameObject.SetActive(true);
+            clientButton.gameObject.SetActive(false);
+            joinCodeButton.onClick.AddListener(async () => {
+                await UnityServices.InitializeAsync();
+                if (!AuthenticationService.Instance.IsSignedIn){
+                    await AuthenticationService.Instance.SignInAnonymouslyAsync();
+                }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCodeInputField.text.ToString());
+                NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+                NetworkManager.Singleton.StartClient();
+            });
+        });
+        hostButton.onClick.AddListener(async () =>
+        {
+            await UnityServices.InitializeAsync();
+            if (!AuthenticationService.Instance.IsSignedIn) {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            }
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(1);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+            var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            var code = NetworkManager.Singleton.StartHost() ? joinCode : null;
+            Debug.Log(code);
+        });
     }
 }
